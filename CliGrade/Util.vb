3c5fabda76e096c59be49931d8389556922effc5
Rijan Chapagain <hrijon.chp@gmail.com>
2020-10-20 04:14:04 +0800

    Adds Utilities for module

---
+++ CliGrade/Util.vb
@@ -0,0 +1,20 @@
+﻿Public Class Util
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub UserInput(ByVal Grade)
+
+        Console.WriteLine("Please Enter your Grade: ")
+        Grade = Console.ReadLine()
+        Console.WriteLine("Your Grade is successfully readed. i.e: ")
+        'Return Grade
+    End Sub
+
+    Public Sub PrintOutput(ByVal Grade)
+        Console.WriteLine("Your Grade is successfully readed. i.e: ", Grade)
+    End Sub
+
+
+End Class
